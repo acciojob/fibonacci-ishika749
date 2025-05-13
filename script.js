31121fb4325f5f32ca77484be9771f8f337d26b7
ishika749 <ishikashah749@gmail.com>
2025-05-13 13:56:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-// program to generate fibonacci series up to n term
+function fibonacci(num) {
+    if (num === 1) return 0;
+    if (num === 2) return 1;
 
-// take input from the user
-const number = parseInt(prompt('Enter the number of terms: '));
-let n1 = 0, n2 = 1, nextTerm;
-
-console.log('Fibonacci Series:');
-
-for (let i = 1; i <= number; i++) {
-    console.log(n1);
-    nextTerm = n1 + n2;
-    n1 = n2;
-    n2 = nextTerm;
+    let a = 0, b = 1;
+    for (let i = 3; i <= num; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
 }
 
+module.exports = fibonacci;
